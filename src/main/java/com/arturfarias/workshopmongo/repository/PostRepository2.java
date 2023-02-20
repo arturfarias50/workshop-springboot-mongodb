0b0929276655439d27162ee107fdb67cf907d40c
arturfarias50 <arturfarias@yahoo.com.br>
2023-02-20 17:23:51 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/arturfarias/workshopmongo/repository/PostRepository2.java
@@ -0,0 +1,11 @@
+package com.arturfarias.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arturfarias.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository2 extends MongoRepository<Post, String> {
+
+}
