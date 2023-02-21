b0ea83943295ac62d5030da6cf248a154f9b09b1
arturfarias50 <arturfarias@yahoo.com.br>
2023-02-21 09:06:29 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/arturfarias/workshopmongo/domain/Post.java
+++ src/main/java/com/arturfarias/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.arturfarias.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.arturfarias.workshopmongo.dto.AuthorDTO;
+import com.arturfarias.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	public String title;
 	public String body;
 	public AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -88,7 +101,4 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(Id, other.Id);
 	}
-	
-	
-
 }
